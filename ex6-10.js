b7dd5f33db3a5f4df4d886917eae03b51ef644d1
santa0708 <103973950+santa0708@users.noreply.github.com>
2023-01-31 22:15:12 +0900

    Delete ex6-10.js

--- ex6-10.js
+++
@@ -1,8 +0,0 @@
-function imgchange1() {
-    document.getElementById('logo').src= 
-    './img/日本ハム.jpeg';
-    }
-    function imgchange2() {
-        document.getElementById('logo').src= 
-        './img/楽天.jpeg';
-        }
