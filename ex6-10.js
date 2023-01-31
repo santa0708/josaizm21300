f9af07d9c7c6525680689d21fe9f78c00c7ba979
santa0708 <103973950+santa0708@users.noreply.github.com>
2023-01-31 22:11:13 +0900

    Create ex6-10.js

---
+++ ex6-10.js
@@ -0,0 +1,8 @@
+function imgchange1() {
+    document.getElementById('logo').src= 
+    './img/日本ハム.jpeg';
+    }
+    function imgchange2() {
+        document.getElementById('logo').src= 
+        './img/楽天.jpeg';
+        }
