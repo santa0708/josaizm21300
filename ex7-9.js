29f4958d1c88aa6ead9dfbb6a473ded18286839e
santa0708 <103973950+santa0708@users.noreply.github.com>
2023-02-01 20:49:20 +0900

    Create ex7-9.js

---
+++ ex7-9.js
@@ -0,0 +1,11 @@
+function nazo5(){
+if(document.getElementById('answer5').value=='村上宗隆'){
+ alert('あたり！');
+}
+else if(document.getElementById('answer5').value=='むらかみむねたか'){
+ alert('あたり！');
+}
+else{
+ alert('はずれ!');
+ }
+}
